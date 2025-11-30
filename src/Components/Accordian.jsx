5afb966161e022289d0161548a020e2525a462a3
Aarya3010 <123272783+Hustler-Aarya@users.noreply.github.com>
2025-12-01 02:44:01 +0530

    add

--- src/Components/Accordian.jsx
+++ src/Components/Accordian.jsx
@@ -2,18 +2,23 @@ import { useState,useEffect } from "react";
 
 
 function Accordian(){
-    const addtodo=()=>{
-        <p>
-            hello 
-        </p>
+    const [toggle, settoggle] = useState(false)
+
+    const open=()=>{
+        settoggle(!toggle)
     }
 
     return (
         <>
         <div className="m-4 p-4 flex flex-col">
             <div className="bg-amber-900 m-4 text-white h-12"
-            onClick={()=>(addtodo(e))}
+            onClick={open}
             >hello</div>
+            {toggle && (
+                <div>
+                    <p>hello how are you</p>
+                </div>
+            )}
             <div className="bg-amber-900 m-4 text-white h-12">hello</div>
             <div className="bg-amber-900 m-4 text-white h-12">hello</div>
             
